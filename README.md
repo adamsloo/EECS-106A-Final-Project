c67b1073f746d74ca2ce66f13bdcf6c9c77de087
adamsloo <adamloo@berkeley.edu>
2023-11-28 19:33:18 -0800

    added working dance moves in the package dance_moves

--- README.md
+++ README.md
@@ -10,18 +10,23 @@ Isabella Borkovic, Heather Ding, Adam Loo, Justin Zhang
 * 
 
 ### dance_moves
-* 
+* originally copy of lab 5 files
+* changed ik_examples.py to move the arm to the three different dance positions
+* changed pick_place.py to repeatedly dance according to the three dance positions
 
 ### find_markers
-* 
+* find_markers.py
+    * Examens all the markers in the workspace (need to tuck so that it can see workspace previous)
+    * Publishes list of markers to topic "/seen_markers"
 
 ### sawyer_full_stack
 * 
 
 ### song_queue
 * pick_up.py
+    * 
 * server.py
+    * flask app that lets users interact with a website and add songs to the queue 
 * upload_photo.py
-    * a
-
-
+    * When this "song_tracker" node receives a new "Marker" message, it adds it to the "current_ids" dictionary. It publishes the song id string message to "song_queue". 
+    * This node add songs we want to play to the queue
--- src/dance_moves/src/move_arm/src/pick_place.py
+++ src/dance_moves/src/move_arm/src/pick_place.py
@@ -16,8 +16,14 @@ def main():
     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
     right_gripper = robot_gripper.Gripper('right_gripper')
 
-    while not rospy.is_shutdown():
+    position_1 = [0.879, -0.462, 0.932, 0.104, 0.426, -0.414, 0.798]
+    position_2 = [0.890, 0.107, 0.221, -0.023, 0.967, 0.008, 0.254]
+    position_3 = [0.727, 0.648, 0.928, -0.214, 0.310, 0.132, 0.917]
+    is_safe = False
+    skip_step = False
+    end_program = False
 
+    while not rospy.is_shutdown() and not end_program:
         input('Press [ Enter ]: ')
         
         # Construct the request
@@ -32,93 +38,93 @@ def main():
         request.ik_request.pose_stamped.header.frame_id = "base"
         
         # Set the desired orientation for the end effector HERE
-
-        request.ik_request.pose_stamped.pose.position.x = 0.591
-        request.ik_request.pose_stamped.pose.position.y = 0.117
-        request.ik_request.pose_stamped.pose.position.z = -0.160    
-        request.ik_request.pose_stamped.pose.orientation.x = 0.0
-        request.ik_request.pose_stamped.pose.orientation.y = 1.0
-        request.ik_request.pose_stamped.pose.orientation.z = 0.0
-        request.ik_request.pose_stamped.pose.orientation.w = 0.0
+        desired_pos = position_2
+        request.ik_request.pose_stamped.pose.position.x = desired_pos[0]
+        request.ik_request.pose_stamped.pose.position.y = desired_pos[1]
+        request.ik_request.pose_stamped.pose.position.z = desired_pos[2]
+        request.ik_request.pose_stamped.pose.orientation.x = desired_pos[3]
+        request.ik_request.pose_stamped.pose.orientation.y = desired_pos[4]
+        request.ik_request.pose_stamped.pose.orientation.z = desired_pos[5]
+        request.ik_request.pose_stamped.pose.orientation.w = desired_pos[6]
         
         try:
-            # Send the request to the service
-            response = compute_ik(request)
-            
-            # Print the response HERE
-            print(response)
-            group = MoveGroupCommander("right_arm")
+            while not is_safe and not skip_step and not end_program:
+                # Send the request to the service
+                response = compute_ik(request)
+                
+                # Print the response HERE
+                print(response)
+                group = MoveGroupCommander("right_arm")
 
-            # Setting position and orientation target
-            group.set_pose_target(request.ik_request.pose_stamped)
+                # Setting position and orientation target
+                group.set_pose_target(request.ik_request.pose_stamped)
 
-            # TRY THIS
-            # Setting just the position without specifying the orientation
-            ###group.set_position_target([0.5, 0.5, 0.0])
-
-            # Plan IK
-            plan = group.plan()
-            user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
-            
-            # Execute IK if safe
-            if user_input == 'y':
-                right_gripper.open()
-                rospy.sleep(1.0)
-                group.execute(plan[1])
-                right_gripper.close()
-                rospy.sleep(1.0)
+                # Plan IK
+                plan = group.plan()
+                user_input = input("Enter 'y' if the trajectory looks safe on RVIZ.\nEnter 's' if you want to skip this step.\nEnter 'e' if you want to end the program.\nEnter any other button to replan the path.\n")
+                # Execute IK if safe
+                if user_input == 'y':
+                    is_safe = True
+                    group.execute(plan[1])
+                    rospy.sleep(1.0)
+                elif user_input == 's':
+                    skip_step = True
+                elif user_input == 'e':
+                    end_program = True
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
 
-        input('Press [ Enter ]: ')
-        
-        # # Construct the request
-        # request = GetPositionIKRequest()
-        # request.ik_request.group_name = "right_arm"
+        # END OF CONFIG
 
-        # # If a Sawyer does not have a gripper, replace '_gripper_tip' with '_wrist' instead
-        # link = "right_gripper_tip"
+        all_positions = [position_3, position_2, position_1, position_2]
+        counter = 0
 
-        # request.ik_request.ik_link_name = link
-        # # request.ik_request.attempts = 20
-        # request.ik_request.pose_stamped.header.frame_id = "base"
-        
-        # Set the desired orientation for the end effector HERE
+        while not end_program:
+            is_safe = False
+            skip_step = False
 
-        request.ik_request.pose_stamped.pose.position.x = 0.533
-        request.ik_request.pose_stamped.pose.position.y = 0.403
-        request.ik_request.pose_stamped.pose.position.z = -0.156    
-        request.ik_request.pose_stamped.pose.orientation.x = 0.0
-        request.ik_request.pose_stamped.pose.orientation.y = 1.0
-        request.ik_request.pose_stamped.pose.orientation.z = 0.0
-        request.ik_request.pose_stamped.pose.orientation.w = 0.0
-        
-        try:
-            # Send the request to the service
-            response = compute_ik(request)
+            input('Press [ Enter ]: ')
+
+            desired_pos = all_positions[counter]
+            counter = (counter + 1) % len(all_positions)
+
+            # Set the desired orientation for the end effector HERE            
+            request.ik_request.pose_stamped.pose.position.x = desired_pos[0]
+            request.ik_request.pose_stamped.pose.position.y = desired_pos[1]
+            request.ik_request.pose_stamped.pose.position.z = desired_pos[2]
+            request.ik_request.pose_stamped.pose.orientation.x = desired_pos[3]
+            request.ik_request.pose_stamped.pose.orientation.y = desired_pos[4]
+            request.ik_request.pose_stamped.pose.orientation.z = desired_pos[5]
+            request.ik_request.pose_stamped.pose.orientation.w = desired_pos[6]
             
-            # Print the response HERE
-            print(response)
-            group = MoveGroupCommander("right_arm")
+            try:
+                while not is_safe and not skip_step and not end_program:
 
-            # Setting position and orientation target
-            group.set_pose_target(request.ik_request.pose_stamped)
+                    # Send the request to the service
+                    response = compute_ik(request)
+                    
+                    # Print the response HERE
+                    print(response)
+                    group = MoveGroupCommander("right_arm")
 
-            # TRY THIS
-            # Setting just the position without specifying the orientation
-            ###group.set_position_target([0.5, 0.5, 0.0])
+                    # Setting position and orientation target
+                    group.set_pose_target(request.ik_request.pose_stamped)
 
-            # Plan IK
-            plan = group.plan()
-            user_input = input("Enter 'y' if the trajectory looks safe on RVIZ")
-            
-            # Execute IK if safe
-            if user_input == 'y':
-                group.execute(plan[1])
-                right_gripper.open()
-                rospy.sleep(1.0)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+                    # Plan IK
+                    plan = group.plan()
+                    user_input = input("Enter 'y' if the trajectory looks safe on RVIZ.\nEnter 's' if you want to skip this step.\nEnter 'e' if you want to end the program.\nEnter any other button to replan the path.\n")
+                    
+                    # Execute IK if safe
+                    if user_input == 'y':
+                        is_safe = True
+                        group.execute(plan[1])
+                        rospy.sleep(1.0)
+                    elif user_input == 's':
+                        skip_step = True
+                    elif user_input == 'e':
+                        end_program = True
+            except rospy.ServiceException as e:
+                    print("Service call failed: %s"%e)
 
 # Python's syntax for a main() method
 if __name__ == '__main__':
