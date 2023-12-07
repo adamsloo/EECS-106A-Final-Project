33f70b91b64d7953913fba82e0ec7a8c7828d61e
heatherding <heather.ding@berkeley.edu>
2023-12-06 21:42:27 -0800

    working on decision processor

--- src/song_queue/src/scripts/decision_processor.py
+++ src/song_queue/src/scripts/decision_processor.py
@@ -1,78 +1,54 @@
-# import rospy
-# from your_package_name.srv import ProcessQueue, ProcessQueueResponse
-# from std_msgs.msg import String
+#!/usr/bin/env python
 
-# class QueueProcessorNode:
-#     def __init__(self):
-#         rospy.init_node('queue_processor_node')
+import rospy
+from std_msgs.msg import String
+import time
+from Queue import Queue  # For Python 2
+from spotify_api import add_song_to_queue
+from song_queue.srv import NewSongRequest
+from pick_and_place import move_cube, move_cube_now_playing
+# If you are using Python 3, use "from queue import Queue" instead
 
-#         # Create a service to process the queue
-#         self.service = rospy.Service('process_queue', ProcessQueue, self.process_queue)
-
-#         # Queue to store messages
-#         self.message_queue = []
-
-#     def process_queue(self, request):
-#         rospy.loginfo("Received queue for processing: %s", request.queue)
-
-#         # Process the queue (for demonstration purposes, just convert strings to uppercase)
-#         processed_queue = [msg.upper() for msg in request.queue]
-
-#         rospy.loginfo("Processed queue: %s", processed_queue)
-
-#         # Return the remaining queue
-#         remaining_queue = [msg for msg in self.message_queue if msg not in processed_queue]
-#         self.message_queue = remaining_queue
-
-#         return ProcessQueueResponse(remaining_queue=remaining_queue)
-
-#     def spin(self):
-#         rospy.spin()
-
-# if __name__ == '__main__':
-#     queue_processor = QueueProcessorNode()
-#     queue_processor.spin()
-
-
-# # class QueueProcessorNode:
-# #     def __init__(self):
-# #         rospy.init_node('queue_processor_node', anonymous=True)
+class QueueProcessorNode:
+    def __init__(self):
+        rospy.init_node('queue_processor_node', anonymous=True)
         
-# #         self.queue = []
-# #         self.processing_time = rospy.get_param('~processing_time', 1.0)  # Default processing time is 1 second
-# #         self.loop_rate = rospy.Rate(10)  # 10 Hz
+        self.queue = []
+        self.processing_time = rospy.get_param('~processing_time', 1.0)  # Default processing time is 1 second
+        self.loop_rate = rospy.Rate(10)  # 10 Hz
+        self.pause_process = 1
 
-# #         rospy.Subscriber('/add_to_queue', NewSongRequest, self.add_to_queue_callback)
-# #         self.subscriber = rospy.Subscriber('/song_changing', bool, self.song_changing_callback)
+        rospy.Subscriber('/add_to_queue', NewSongRequest, self.add_to_queue_callback)
+        self.subscriber = rospy.Subscriber('/song_changing', bool, self.song_changing_callback)
     
-# #     def song_changing_callback(self, msg):
-# #         # Callback function that stores the received message
-# #         self.received_message = msg.data
+    def song_changing_callback(self, msg):
+        # Callback function that stores the received message
+        self.pause_process = self.pause_process * -1
 
-# #     def add_to_queue_callback(self, data):
-# #         rospy.loginfo('Adding to queue: %s', data.ar_tag, data.song)
-# #         self.queue.append([data.ar_tag, data.song])
+    def add_to_queue_callback(self, data):
+        rospy.loginfo('Adding to queue: %s', data.ar_tag, data.song)
+        self.queue.append([data.ar_tag, data.song])
 
-# #     def process_queue(self):
-# #         while not rospy.is_shutdown():
-# #             if len(self.queue) > 0:
-# #                 ar_tag, song = self.queue.pop(0)
-# #                 add_song_to_queue(song) 
-# #                 # self.taken_tags.remove(ar_tag)
-# #                 # self.available_tags.append(ar_tag)
-# #                 print("MOVING CUBE")
-# #                 self.move_cube(int(ar_tag), int(self.prev_ar_tag))
-# #                 self.prev_ar_tag = ar_tag
+    def process_queue(self):
+        while not rospy.is_shutdown() and self.pause_process == 1:
+            if len(self.queue) > 0:
+                ar_tag, song = self.queue.pop(0)
+                add_song_to_queue(song) 
+                # self.taken_tags.remove(ar_tag)
+                # self.available_tags.append(ar_tag)
+                print("MOVING CUBE")
+                self.move_cube(int(ar_tag), int(self.prev_ar_tag))
+                self.prev_ar_tag = ar_tag
 
-# #                 #time how long move cube takes
-# #                 time.sleep(self.processing_time)
+                #time how long move cube takes
+                time.sleep(self.processing_time)
 
-# #             # Check if there is time left, otherwise wait
-# #             self.loop_rate.sleep()
+            # Check if there is time left, otherwise wait
+            self.loop_rate.sleep()
 
-# # if __name__ == '__main__':
-# #     try:
-# #         node = QueueProcessorNode()
-# #         node.process_queue()
-# #     except rospy.ROSInterruptException:
-# #         pass
+if __name__ == '__main__':
+    try:
+        node = QueueProcessorNode()
+        node.process_queue()
+    except rospy.ROSInterruptException:
+        pass
